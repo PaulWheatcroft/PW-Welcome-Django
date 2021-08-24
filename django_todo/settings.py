3d859b376d260df6a69bd5b6bb0abc80d9a44beb
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-08-24 15:51:26 +0000

    Ready for Heroku deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://cyeujjaxfvzzql:72b18f71e8166176515304de349f949072abd26f9da5ab8ad3a3e5ffc8680544@ec2-34-249-247-7.eu-west-1.compute.amazonaws.com:5432/d5hupsqvd1rmjl')
 }
 
 
